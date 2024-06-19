665218a3da5d49704d25d22a13dffecadce5861f
wfnuser <wfnuser@hotmail.com>
2024-06-19 21:24:56 +0800

    update test for bet

--- youbet/test/Bet.t.sol
+++ youbet/test/Bet.t.sol
@@ -5,9 +5,13 @@ import {Test, console} from "forge-std/Test.sol";
 import {Bet} from "../src/Bet.sol";
 
 contract BetTest is Test {
-    Bet public Bet;
+    Bet public bet;
 
     function setUp() public {
         bet = new Bet();
+    }
+
+    function testCreateGoal() public {
+        bet.createGoal("Test Goal", "This is a test goal", 1 ether, 5);
     }
 }
