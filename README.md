916d43e9c6bb0bbbbf283101d083d64a3f9e32bd
feynman <drolmen7@gmail.com>
2024-10-12 20:27:59 +0800

    feat: refactor the script to support multiple chains

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # youbet
+
 You Bet! We can pay for everything in flow.
+
+## Deploy Script
+
+Contract Deploy: `./deploy_verify.sh <RPC_URL> <VERIFIER> [BLOCK_SCOUT_URL]`
+
+Contract Upgrade: `./upgrade_verify.sh <RPC_URL> <PROXY_ADDRESS> <VERIFIER> [BLOCK_SCOUT_URL]`
+
+Deploy Example:
+
+```bash
+// deploy and verify by etherscan
+./deploy_verify.sh https://rpc.xxx etherscan
+
+// deploy and verify by blockscout
+./deploy_verify.sh https://rpc.xxx blockscout https://blockscout.xxx
+```
+
+Upgrade Example:
+
+```bash
+// upgrade and verify by etherscan
+./upgrade_verify.sh  https://rpc.xxx  0x4A91xxxx etherscan
+
+// upgrade and verify by etherscan
+./upgrade_verify.sh  https://rpc.xxx  0x4A91xxxx blockscout https://blockscout.xxx
+```
--- youbet/upgrade_verify.sh
+++ youbet/upgrade_verify.sh
@@ -1,13 +1,27 @@
-forge clean 
+set -e
+
+export RPC_URL="$1"
+export PROXY_ADDRESS="$2"
+export VERIFIER="$3"
+export BLOCK_SCOUT_URL="$4"
 
 source .env 
 
+VERIFIER_ARGS=""
+if [ "$VERIFIER" = "blockscout" ]; then
+    VERIFIER_ARGS="--verifier-url $BLOCK_SCOUT_URL/api/"
+elif [ "$VERIFIER" = "etherscan" ]; then
+    VERIFIER_ARGS="--etherscan-api-key $ETHERSCAN_API_KEY"
+fi
+
+forge clean 
+
 forge script \
     script/BetUpgrade.s.sol:BetScript \
-    --rpc-url https://rpc.open-campus-codex.gelato.digital \
-    -g 20000 \
+    --rpc-url  $RPC_URL \
+    --skip-simulation \
     --broadcast \
     --verify \
-    --verifier blockscout \
-    --verifier-url https://opencampus-codex.blockscout.com/api/ \
+    --verifier $VERIFIER \
+    $VERIFIER_ARGS \
     -vvvv
