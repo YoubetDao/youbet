f49949de643ebee8d9b90c4585fc3839136eabda
hunknownz <klercc37@gmail.com>
2025-07-24 01:44:15 +0800

    fix: skip storage laye check for first deployment
    Signed-off-by: hunknownz <klercc37@gmail.com>

--- youbet/script/DistributorDeploy.s.sol
+++ youbet/script/DistributorDeploy.s.sol
@@ -1,25 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console} from "forge-std/Script.sol";
-import {Distributor} from "../src/Distributor.sol";
-import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import {Script, console} from 'forge-std/Script.sol';
+import {Distributor} from '../src/Distributor.sol';
+import {Upgrades, Options} from 'openzeppelin-foundry-upgrades/Upgrades.sol';
 
 contract DistributorScript is Script {
     function run() public {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        uint256 deployerPrivateKey = vm.envUint('PRIVATE_KEY');
         address ownerAddress = vm.addr(deployerPrivateKey);
-        address signerAddress = vm.envAddress("SIGNER_ADDRESS");
-        address tokenAddress = vm.envAddress("TOKEN_ADDRESS");
+        address signerAddress = vm.envAddress('SIGNER_ADDRESS');
+        address tokenAddress = vm.envAddress('TOKEN_ADDRESS');
         vm.startBroadcast(deployerPrivateKey);
 
-        Upgrades.deployUUPSProxy(
-            "Distributor.sol:Distributor",
+        Options memory opts;
+        opts.unsafeSkipStorageCheck = true;
+
+        address proxy = Upgrades.deployUUPSProxy(
+            'Distributor.sol:Distributor',
             abi.encodeCall(
                 Distributor.initialize,
                 (signerAddress, ownerAddress, tokenAddress)
-            )
+            ),
+            opts
         );
+
+        console.log('Distributor proxy deployed at:', proxy);
 
         vm.stopBroadcast();
     }
